25728b5c9914b3d2335e47207dcc395beed43e75
Silent Node <109953530+muhrobby@users.noreply.github.com>
2026-09-02 15:04:29 +0700

    Add configuration for Warehouse App

---
+++ js/config.js
@@ -0,0 +1,17 @@
+window.APP_CONFIG = Object.freeze({
+
+  APP_NAME: 'Warehouse App',
+
+  API_BASE_URL:
+    'https://n8n-v2.humalab.my.id/webhook',
+
+  SESSION_KEY:
+    'warehouse_session',
+
+  SESSION_TTL_MS:
+    8 * 60 * 60 * 1000,
+
+  REQUEST_TIMEOUT_MS:
+    15000
+
+});
